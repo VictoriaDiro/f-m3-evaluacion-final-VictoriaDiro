1cde3d1f0f2355fa606946190d1c3266787f152d
VictoriaDiro <victoriadiro01@gmail.com>
2019-05-27 09:36:00 +0200

    fix reload page error

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -17,19 +17,28 @@ class CharacterCard extends React.Component {
 
     const potterId = this.props.match.params.id;
     const potterItem = potterList[potterId];
-    
+
     return(
       <React.Fragment>
 
         <div className="card">
-          <div className="image__card" style={{ backgroundImage: `url(${potterItem.image})`}} alt={potterItem.name} />
-          <div className="info__container">
-            <h2 className="name__card">{potterItem.name}</h2>
-            <h3 className="house__card">{`Casa: ${potterItem.house}`}</h3>
-            <h3 className="year__card">{`Año de nacimiento: ${potterItem.yearOfBirth}`}</h3>
-            <h3 className="patronus__card">{`Patronus: ${potterItem.patronus}`}</h3>
-            <h3 className="state__card">Estado: {potterItem.alive ? <i className="fas fa-heartbeat"></i> : <i className="fas fa-skull"></i>}</h3>
-          </div>
+
+          {potterList.length < 1 ?
+            <p classme="no__results">Sin resultados</p>
+            :
+            <div className="card__container">
+              <div className="image__card" style={{ backgroundImage: `url(${potterItem.image})`}} alt={potterItem.name}
+              />
+
+              <div className="info__container">
+                <h2 className="name__card">{potterItem.name}</h2>
+                <h3 className="house__card">{`Casa: ${potterItem.house}`}</h3>
+                <h3 className="year__card">{`Año de nacimiento: ${potterItem.yearOfBirth}`}</h3>
+                <h3 className="patronus__card">{`Patronus: ${potterItem.patronus}`}</h3>
+                <h3 className="state__card">Estado: {potterItem.alive ? <i className="fas fa-heartbeat"></i> : <i className="fas fa-skull"></i>}</h3>
+              </div>
+            </div>
+          }
         </div>
 
         <Link to="/" className="return__link">Volver</Link>
