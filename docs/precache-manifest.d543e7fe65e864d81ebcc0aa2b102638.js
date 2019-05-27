219f15d05c537eb3eb140cf797216349d439244b
VictoriaDiro <victoriadiro01@gmail.com>
2019-05-27 19:31:34 +0200

    update docs

---
+++ docs/precache-manifest.d543e7fe65e864d81ebcc0aa2b102638.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1aeae970c81a4c86dab3cbac78e4456",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f0408eda4699d3d41d34",
+    "url": "./static/css/main.985f2688.chunk.css"
+  },
+  {
+    "revision": "87799f3c5a9d7a4a5460",
+    "url": "./static/js/2.a7e49364.chunk.js"
+  },
+  {
+    "revision": "f0408eda4699d3d41d34",
+    "url": "./static/js/main.e3860e08.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "c0d48dc10684427bcb4262040fc368f7",
+    "url": "./static/media/HarryPotterLogo.c0d48dc1.png"
+  },
+  {
+    "revision": "08f909d5d68f07e63fd382931ecc8ce1",
+    "url": "./static/media/Hogwarts.08f909d5.jpg"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f51310c3c2dd725ab6d4de415e8520e.js"
+  "./precache-manifest.d543e7fe65e864d81ebcc0aa2b102638.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
